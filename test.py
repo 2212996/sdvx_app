7d48a6733fdba9b4ea32da8a784d2286587cdbfb
2212996 <51160826+2212996@users.noreply.github.com>
2021-11-10 14:33:18 +0900

    竹の白スコア

--- test.py
+++ test.py
@@ -7,11 +7,14 @@ url = 'https://nearnoah.net/api/showUserData.json?username=221sdvx'
 
 response = requests.get(url)
 
-# print(response.text)
-
 d = json.loads(response.text)
 
 # 曲データだけ取り出し
 trackdata = d['profile']['tracks']
 
-pprint.pprint(trackdata, width=40)
+# 竹の白譜面のスコアを返すテスト
+take = trackdata[1710]
+take_score = take['maximum']['score']
+print(take_score)
+
+# pprint.pprint(trackdata[1710], width=40)
