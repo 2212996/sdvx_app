b7b2e0c0d4530fa6cb41093b30c3e04dff2a29e0
2212996 <51160826+2212996@users.noreply.github.com>
2021-11-11 15:54:02 +0900

    任意の難易度の曲名とスコアを返すやつ

--- test.py
+++ test.py
@@ -12,9 +12,21 @@ d = json.loads(response.text)
 # 曲データだけ取り出し
 trackdata = d['profile']['tracks']
 
-# 竹の白譜面のスコアを返すテスト
-take = trackdata[1710]
-take_score = take['maximum']['score']
-print(take_score)
+# 任意の難易度の楽曲を取り出す
 
-# pprint.pprint(trackdata[1710], width=40)
+#　難易度の変数
+difficulty = 19
+
+# 難易度の名前一覧の配列
+diff_rank = ['novice', 'advanced', 'exhaust', 'infinite', 'gravity', 'heavenly', 'vivid', 'maximum']
+
+for i in range(len(trackdata)):
+    tmp = trackdata[i]
+    for j in range(len(diff_rank)):
+        if(diff_rank[j] in tmp):
+            tmp_dif = tmp[diff_rank[j]]
+            if('score' in tmp_dif and tmp[diff_rank[j]]['level'] == difficulty):
+                print(tmp['title'] + ' : ' + str(tmp[diff_rank[j]]['score']))
+
+# 綺麗な形式でjsonを出してくれるやつ
+# pprint.pprint(trackdata, width=40)
