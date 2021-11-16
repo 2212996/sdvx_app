15610be052f5ec68a0cf180118e3fa90afb87c0a
2212996 <51160826+2212996@users.noreply.github.com>
2021-11-16 16:10:00 +0900

    api.py頑張り part2

--- backend/api.py
+++ backend/api.py
@@ -14,9 +14,9 @@ def save_from_API(username):
         json.dump(rawdata, f, ensure_ascii=False)
 
 def open_json(username):
-    with open(username + '_score.json') as f:
-        rawdata = json.load(f)
-    return rawdata
+    with open(username + '_score.json', 'rb') as fin:
+        d = json.load(fin)
+    return d
 
 # 任意のユーザーのスコアリストをAPIのから取得する
 def receive_from_API(username):
@@ -85,7 +85,7 @@ def to_narrowdown_songs(scorebox, minlevel, maxlevel, clearlamp, grade):
 
     # clearlampの絞り込みが無かった場合、全てにチェックを付ける
     if(len(clearlamp) == 0):
-        clearlamp = ['NOPLAY', 'CRASH', 'COMP', 'EXCOMP', 'UC', 'PUC']
+        clearlamp = ['NOPLAY', 'CRASH', 'COMP', 'EX COMP', 'UC', 'PER']
 
     # gradeの絞り込みが無かった場合、全てにチェックを付ける
     if(len(grade) == 0):
@@ -96,23 +96,63 @@ def to_narrowdown_songs(scorebox, minlevel, maxlevel, clearlamp, grade):
         tmp_scorebox = scorebox[i]
         if(
             not(tmp_scorebox['level'] >= minlevel and tmp_scorebox['level'] <= maxlevel)
-            or (not(tmp_scorebox['clearlamp'] in clearlamp))
-            or (not(tmp_scorebox['grade'] in grade))
+            or not(tmp_scorebox['clearlamp'] in clearlamp)
+            or not(tmp_scorebox['grade'] in grade)
             ):
             del scorebox[i]
 
     return scorebox
+
+def make_id_list(scorebox):
+
+    id_list = set()
+
+    for i in range(len(scorebox)):
+        tmp_scorebox = scorebox[i]
+        id_list.add(tmp_scorebox['id'])
+
+    return id_list
+
+def to_narrowdown_byID(scorebox, id_list):
+
+    narrowed_scorebox = []
+    a = 0
+    for i in range(len(scorebox)):
+        tmp_scorebox = scorebox[i]
+        if(tmp_scorebox['id'] == id_list[a]):
+            narrowed_scorebox.append(tmp_scorebox)
+            a = a + 1
+
+    return narrowed_scorebox
 
 def cals_scoregap(scorebox1, scorebox2):
 
     id_1 = []
     id_2 = []
 
+    id_1 = make_id_list(scorebox1)
+    id_2 = make_id_list(scorebox2)
+
+    id_intersection = id_1 & id_2
+
+    id_listed = list(id_intersection)
+
+    scorebox1 = to_narrowdown_byID(scorebox1, id_listed)
+    scorebox2 = to_narrowdown_byID(scorebox2, id_listed)
+
+    scoregap = []
     for i in range(len(scorebox1)):
+        user1_song = scorebox1[i]
+        user2_song = scorebox2[i]
+        diff = user1_song['score'] - user2_song['score']
+
+        scoregap.append({'title': user1_song['title'], 'gap': diff})
+
+    return scoregap
+
+def to_narrowdown_byGap(scoregap, gap):
+    for i in range(len(scoregap)):
         
-
-
-
 
 # 以下、テストケース
 
@@ -142,14 +182,19 @@ def cals_scoregap(scorebox1, scorebox2):
 
 # a = receive_from_API("221sdvx")
 
-a = open_json('221sdvx')
-b = organize_json(a)
+a221 = open_json('221sdvx')
+b221 = organize_json(a221)
+adas = open_json('ddr_das')
+bdas = organize_json(adas)
 
 minlevel = 18
 maxlevel = 18
-clearlamp = ['PER']
-grade = ['S']
+clearlamp = []
+grade = []
 
-c = to_narrowdown_songs(b, minlevel, maxlevel, clearlamp, grade)
+c221 = to_narrowdown_songs(b221, minlevel, maxlevel, clearlamp, grade)
+cdas = to_narrowdown_songs(bdas, minlevel, maxlevel, clearlamp, grade)
 
-pprint.pprint(c)
+d = cals_scoregap(c221, cdas)
+
+print(d)
